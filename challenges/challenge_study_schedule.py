6026b39e638afb67a4894ad5941477bf1cf03058
hector <hectoreddbenitez@gmail.com>
2022-10-10 17:47:09 -0300

    feat(req01): implementa função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+
+    contador = 0
+    try:
+        for tupla in permanence_period:
+            if tupla[0] <= target_time <= tupla[1]:
+                contador += 1 
+    except TypeError:
+        return None
+    return contador
+
+
+
