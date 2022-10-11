31275b915d7eb6461377d49575dad6d4382e7238
hector <hectoreddbenitez@gmail.com>
2022-10-11 14:40:00 -0300

    fix(req03): fix lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -12,8 +12,8 @@ def selection_sort(string):
 
     return string
 
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
     if not first_string or not second_string:
         return False
     if len(first_string) != len(second_string):
