f1f49589ca0e2c30c0a3f7fdbe877bf134c6e7d2
hector <hectoreddbenitez@gmail.com>
2022-10-11 14:20:28 -0300

    feat(req03): implemeta função is_anagram. Complexidade acima do permitido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,26 @@
+def selection_sort(string):
+    n = len(string)
+    for index in range(n - 1):
+        min_index = index
+        for search_index in range(index + 1, n):
+            if string[search_index] < string[min_index]:
+                min_index = search_index
+
+        current_element = string[index]
+        string[index] = string[min_index]
+        string[min_index] = current_element
+
+    return string
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    else:
+        first_string_sorted = selection_sort(list(first_string.lower()))
+        second_string_sorted = selection_sort(list(second_string.lower()))
+        if first_string_sorted != second_string_sorted:
+            return False
+        return True
