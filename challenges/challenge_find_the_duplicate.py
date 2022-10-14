bde5e2794ae2ce6528adbe613b9f7ee2ca2e71dd
hector <hectoreddbenitez@gmail.com>
2022-10-14 13:36:59 -0300

    feat(req04): bonus - implementa find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    nums.sort()
+    for index in range(0, len(nums) -1):
+        if type(nums[index]) == str or nums[index] < 0:
+                return False
+
+        if nums[index] == nums[index + 1]:
+                return nums[index]
+
+    return False
+print(find_duplicate([2, 3, 1, 5, 5, 6, 8, 9]))
