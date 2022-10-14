1a33616c728c4af3b8c2b9a863034f9aabeea6b9
hector <hectoreddbenitez@gmail.com>
2022-10-14 20:26:52 -0300

    feat(req05): bonus - implementa is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    start = 0
+    end = len(word) - 1
+    if not word:
+        return False
+    while start <= end:
+            if word[start] != word[end]:
+                return False
+            else:
+                start = start + 1
+                end = end - 1
+                    
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,4 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
     # condisão de parada
     if not word or word[low_index] != word[high_index]:
         return False
