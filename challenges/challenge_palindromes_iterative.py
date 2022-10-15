79b0048f1cf921378897870f0185b163eabda4af
hector <hectoreddbenitez@gmail.com>
2022-10-14 21:44:43 -0300

    feat(req05): bonus - upgrade

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,13 +1,18 @@
 def is_palindrome_iterative(word):
-    start = 0
-    end = len(word) - 1
-    if not word:
-        return False
-    while start <= end:
-            if word[start] != word[end]:
-                return False
-            else:
-                start = start + 1
-                end = end - 1
-                    
-    return True
+    # start = 0
+    # end = len(word) - 1
+    # if not word:
+    #     return False
+    # while start <= end:
+    #         if word[start] != word[end]:
+    #             return False
+    #         else:
+    #             start = start + 1
+    #             end = end - 1
+    # return True
+
+    # Pythonic way!!
+    if not word: return False
+    else:
+        if word == word[::-1]: return True             
+    return False
